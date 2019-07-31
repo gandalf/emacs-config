9359b6b41d512ef17bef32316bb4c9cb717a45a5
Anthoni Shogan <anthoni@gondolin.localdomain>
2019-07-31 18:12:06 +0530

    Fixed flycheck on web-mode.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,4 +1,16 @@
 ;;; lisp/init-flycheck.el
 (use-package flycheck
-  :init (global-flycheck-mode))
+  ;;;:init (global-flycheck-mode)
+
+  :config
+  ;; disable default jslint
+  (setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint json-jsonlist)))
+
+  ;; enable flycheck
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  )
+
 (provide 'init-flycheck)
